a4591bebc2544195935d87e5f64407b3d37cdf7c
rssa1230 <117625366+rssa1230@users.noreply.github.com>
2023-01-14 15:37:56 +0700

    Add files via upload

--- osismpk/urls.py
+++ osismpk/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from pengurus.views import data
+from pengurus.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('data/', data),
+    path('tambah-data/', tambah_data),
+    path('data/ubah/<int:id_data>', ubah_data, name='ubah_data'),
 ]
