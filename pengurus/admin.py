ead531cf7699297c5968bf086fecaaaa9d4c1e62
rssa1230 <117625366+rssa1230@users.noreply.github.com>
2023-01-14 15:40:26 +0700

    Add files via upload

--- pengurus/admin.py
+++ pengurus/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from pengurus.models import *
 
-# Register your models here.
+class DataAdmin(admin.ModelAdmin):
+    list_display = ['nis', 'nama', 'kelas', 'jabatan']
+    search_fields = ['nis', 'nama', 'kelas', 'jabatan']
+    list_filter = ('keterangan_id',)
+    list_per_page = 4
+
+admin.site.register(Data, DataAdmin)
+admin.site.register(Keterangan)
+
